dea653ea6558b9e41ced6a9f7515a3878ae44bb1
Aadarsh Pandey <aadarshpandey.pandey@gmail.com>
2023-02-18 19:08:05 +0530

    Added Routes for Nav Purpose

---
+++ lib/Routes/routes.dart
@@ -0,0 +1,5 @@
+class Routes{
+  static String LoadingScreen = "/loading";
+  static String IntroScreen = "/intro";
+  static String SplashScreen = "/splash";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,13 @@ class AroundU extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: IntroPage(),
+      initialRoute: '/intro',
+      routes: {
+        '/loading': (context) => const LoadingScreen(),
+        '/intro': (context) => const IntroPage(),
+      },
     );
   }
 }
